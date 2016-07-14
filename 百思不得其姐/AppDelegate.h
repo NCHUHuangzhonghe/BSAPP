a335630a8e80f74c7116c5804212d19dac0031ae
NCHUHuangzhonghe <huangzhonghe@yahoo.com>
2016-07-14 19:17:29 +0800

    Initial Commit

---
+++ 百思不得其姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得其姐
+//
+//  Created by huangzhonghe on 16/7/14.
+//  Copyright © 2016年 huangzhonghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
